0cd60370af9bb1a08ee5733cefdb55b51c60905e
bhrouz <bhroz1234@yahoo.com>
2022-08-15 15:10:04 +0200

    Add project files.

---
+++ MVC1/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC1.Controllers
+    {
+    public class HomeController : Controller
+        {
+        public IActionResult Index()
+            {
+            return View();
+            }
+
+        public IActionResult Privacy()
+            {
+            return View();
+            }
+        public IActionResult About()
+            {
+            return View();
+            }
+        public IActionResult Contact()
+            {
+            return View();
+            }
+        public IActionResult Projects()
+            {
+            return View();
+            }
+
+        }
+    }
