c376600c9b393250bab76d938b2b384cee281078
bhrouz <bhroz1234@yahoo.com>
2022-08-15 15:40:29 +0200

    v2

---
+++ MVC1/Controllers/Doctor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC1.Controllers
+    {
+    public class Doctor : Controller
+        {
+        public IActionResult Index()
+            {
+            return View();
+            }
+        }
+    }
