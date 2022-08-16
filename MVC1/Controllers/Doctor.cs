a7615e5f34d1b23ae2047ad16a519f9a5f6ed3ca
bhrouz <bhroz1234@yahoo.com>
2022-08-16 14:51:18 +0200

    v2

---
+++ MVC1/Controllers/Doctor.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC1.Models;
+
+namespace MVC1.Controllers
+    {
+    public class Doctor : Controller
+        {
+
+
+        public string grad { get; set; }
+        public IActionResult fever()
+            {
+            if (!String.IsNullOrEmpty(grad)) {
+                ViewBag.grad = grad;
+                return View("fever");
+                }
+            else
+                return View();
+            }
+        [HttpPost]
+        public IActionResult fever(string grad)
+            {
+            if (!String.IsNullOrEmpty(grad))
+                {
+                ViewBag.grad = grad;
+                return View("fever");
+                }
+            else
+                return View("fever");
+            
+            }
+
+        
+
+    }
+    }
