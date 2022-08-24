dc70b2e9c9ace51bee7214f4b123d7b04a3d1e41
bhrouz <bhroz1234@yahoo.com>
2022-08-24 10:41:33 +0200

    v3

--- MVC1/Models/People.cs
+++ MVC1/Models/People.cs
@@ -1,7 +1,19 @@
-﻿namespace MVC1.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC1.Models
 {
     public class People
     {
+        [Required]
+        public int Id;
+        [Required]
+        public string First_name;
+        [Required]
+        public string Last_name;
+        [Required]
+        public string Country;
+        [Required]
+        public string Tel;
         public IList<Person> people;
     }
 }
