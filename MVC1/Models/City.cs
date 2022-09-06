b54c4911173acc8e85ee98f9329f34736c0eeda3
bhroz damsaz <bhroz1234@yahoo.com>
2022-09-06 23:22:09 +0200

    cc2

--- MVC1/Models/City.cs
+++ MVC1/Models/City.cs
@@ -4,12 +4,11 @@ namespace MVC1.Models
 {
     public class City
     {
-        [Key]
+      public int id { get; set; }   
         public string CityName { get; set; }
-        public string lat { get; set; }
-        public string lng { get; set; }
         public string CountryId { get; set; }
         public Country Country;
-        public IList<Person> people { get; set; }
-        }
+       
+        public ICollection<Person> people { get; set; }
+    }
 }
